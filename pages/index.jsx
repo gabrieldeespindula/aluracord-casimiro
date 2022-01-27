1ec2c6fa1ce101a04d12c6a47f0e7db1190d5362
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2022-01-26 21:40:52 -0300

    conteúdos passados na aula 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-	return (
-		<style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-	);
-}
 
 function Title(props) {
 	const Tag = props.tag || 'h1';
@@ -47,11 +21,11 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-	const username = 'gabrieldeespindula';
+	const [username, setUsername] = React.useState('');
+	const router = useRouter();
 
 	return (
 		<>
-			<GlobalStyle />
 			<Box
 				styleSheet={{
 					display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -98,6 +72,12 @@ export default function PaginaInicial() {
 					{/* Formulário */}
 					<Box
 						as="form"
+						onSubmit={
+							function (ev) {
+								ev.preventDefault();
+								router.push('/chat');
+							}
+						}
 						styleSheet={{
 							display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
 							width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -109,6 +89,13 @@ export default function PaginaInicial() {
 						</Text>
 
 						<TextField
+							value={username}
+							onChange={
+								function (event) {
+									const value = event.target.value;
+									setUsername(value);
+								}
+							}
 							fullWidth
 							textFieldColors={{
 								neutral: {
